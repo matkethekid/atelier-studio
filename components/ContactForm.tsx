d82405b229a07a176fa93e4f5d4445bf7fd98769
mateja <markojovanovicc23@outlook.com>
2026-04-07 18:21:52 +0200

    web3forms & hcaptcha added

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
+import toast, { Toaster } from "react-hot-toast";
 
 interface Language {
     id: number;
@@ -14,24 +17,53 @@ interface Props {
 };
 
 const ContactForm = ({ languages }: Props) => {
-    const [name, setName] = useState<string>("");
-    const [email, setEmail] = useState<string>("");
     const [lang, setLang] = useState<string>("");
-    const [message, setMessage] = useState<string>("");
+    const [hToken, setHToken] = useState<string | null>(null);
 
-    async function sendMessage() {
-        console.log("ee");
-    }
+    const { register, handleSubmit, setValue } = useForm();
+
+    const onHCaptchaChange = (token: string) => {
+        setHToken(token);
+        setValue("h-captcha-response", token);
+    };
+
+    const onSubmit = async (data: any) => {
+        if (!hToken) {
+            toast.error("Molimo popunite captcha.");
+            return;
+        }
+
+        const formData = {
+            access_key: process.env.NEXT_PUBLIC_WEB3FORMS_SITEKEY, // ubaci svoj Web3Forms key
+            subject: `Poruka sa sajta - ${data.name}`,
+            language: lang,
+            ...data,
+        };
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+        });
+
+        const json = await res.json();
+        if (json.success) {
+            toast.success("Poruka poslata!");
+        } else {
+            toast.error("Došlo je do greške, pokušajte ponovo.");
+        }
+    };
   return (
-    <form className='lg:p-5 flex flex-col gap-10'>
+    <form className='lg:p-5 flex flex-col gap-10' onSubmit={handleSubmit(onSubmit)}>
+        <Toaster/>
         <div className='flex flex-col lg:flex-row gap-3'>
             <div className='flex flex-col gap-2'>
                 <label>Ime</label>
-                <input type="text" placeholder='Petar Petrović' className='bg-[#FBFAEE] p-3 rounded-lg' onChange={(e) => setName(e.target.value)}/>
+                <input type="text" placeholder='Petar Petrović' className='bg-[#FBFAEE] p-3 rounded-lg' {...register("name", { required: true })}/>
             </div>
             <div className='flex flex-col gap-2'>
                 <label>Email</label>
-                <input type="email" placeholder='petar@petrovic.com' className='bg-[#FBFAEE] p-3 rounded-lg' onChange={(e) => setEmail(e.target.value)}/>
+                <input type="email" placeholder='petar@petrovic.com' className='bg-[#FBFAEE] p-3 rounded-lg' {...register("email", { required: true })}/>
             </div>
         </div>
         <div className='flex flex-col gap-3'>
@@ -46,9 +78,14 @@ const ContactForm = ({ languages }: Props) => {
         </div>
         <div className='flex flex-col gap-2'>
             <label>Vaša poruka</label>
-            <textarea placeholder="Vaša poruka ovde..." className='bg-[#FBFAEE] p-3 rounded-lg min-h-50' onChange={(e) => setMessage(e.target.value)}></textarea>
+            <textarea placeholder="Vaša poruka ovde..." className='bg-[#FBFAEE] p-3 rounded-lg min-h-50' {...register("message", { required: true })}></textarea>
         </div>
-        <button aria-label="Pošalji poruku" onClick={sendMessage} className="w-full p-5 rounded-xl text-white lg:max-w-50 bg-[#d66140] cursor-pointer hover:bg-[#ca5839]">Pošalji poruku</button>
+        <HCaptcha
+            sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+            reCaptchaCompat={false}
+            onVerify={onHCaptchaChange} 
+        />
+        <button aria-label="Pošalji poruku" type="submit" className="w-full p-5 rounded-xl text-white lg:max-w-50 bg-[#d66140] cursor-pointer hover:bg-[#ca5839]">Pošalji poruku</button>
     </form>
   )
 }
