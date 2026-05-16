688f07cb8126f95ebdd4eb0124058c9dfbb7e7f2
Mateja <145758273+matkethekid@users.noreply.github.com>
2026-05-16 15:25:07 +0200

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://atelier-studio-kohl-seven.vercel.app/sitemap.xml',
+    sitemap: 'https://atelierstudio.rs/sitemap.xml',
   }
 };
