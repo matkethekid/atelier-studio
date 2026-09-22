15314fba42749fc79be846f1b9cb76c958027941
matkethekid <markojovanovicc23@outlook.com>
2026-09-22 20:08:55 +0200

    feat: add orderFreeDownload method

--- actions/downloads.ts
+++ actions/downloads.ts
@@ -1,9 +1,29 @@
-﻿export const fetchResources = async () => {
+﻿interface FreeDownloadParams {
+    id: string;
+    customerEmail: string;
+}
+
+export const fetchResources = async () => {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/downloads/all`);
         const data = await res.json();
-        console.log(data.data);
         return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const orderFreeDownload = async (params: FreeDownloadParams) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/downloads/download`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(params),
+        });
+        const data = await res.json();
+        return data;
     } catch (error) {
         console.log(error);
     }
